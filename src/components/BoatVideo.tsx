dc6642040435495e68bcb82c99cb90a3faa9451e
Sam Murphy <61816889+itssammurphy@users.noreply.github.com>
2023-12-18 21:05:59 +1100

    Update BoatVideo.tsx
    Swapped order of precedence for file formats

--- src/components/BoatVideo.tsx
+++ src/components/BoatVideo.tsx
@@ -1,7 +1,7 @@
 const BoatVideo = () => {
     return <video className="h-100vh w-full object-cover animate-clip-path-from-top" autoPlay muted loop playsInline webkit-playsinline={true}>
-        <source src="/assets/boat_opt.mp4" type="video/mp4"/>
         <source src="/assets/boat.webm" type="video/webm"/>
+        <source src="/assets/boat_opt.mp4" type="video/mp4"/>
     </video>
 }
 
