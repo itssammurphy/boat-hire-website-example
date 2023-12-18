c4f20967b6aa9390d20592f0a2e8b2da166ecfb1
Sam Murphy <samuelm.business@gmail.com>
2023-12-18 20:23:57 +1100

    fixed video bug

--- src/components/BoatVideo.tsx
+++ src/components/BoatVideo.tsx
@@ -1,6 +1,6 @@
 const BoatVideo = () => {
     return <video className="h-100vh w-full object-cover animate-clip-path-from-top" autoPlay muted loop>
-        <source src="src/assets/boat.mp4" type="video/mp4"/>
+        <source src="/assets/boat.mp4" type="video/mp4"/>
     </video>
 }
 
