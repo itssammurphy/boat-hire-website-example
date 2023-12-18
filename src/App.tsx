f4fc08448caea3569fc68bb05073e686cb47bedc
Sam Murphy <samuelm.business@gmail.com>
2023-12-18 20:20:49 +1100

    removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Navbar from './components/Navbar'
 import Home from './Home'
-import Boat from './components/Boat'
 
 function App() {
   return (
